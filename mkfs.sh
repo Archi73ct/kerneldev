ae64f9c7851bf33ea1b524f5b21567e5fa839b3b
Archi73ct <jbechsen@gmail.com>
2022-05-20 00:04:23 +0200

    fixed dumb stuff

--- mkfs.sh
+++ mkfs.sh
@@ -16,13 +16,13 @@ echo "[+] Building filesystem..."
 mkdir fs
 pushd fs
 echo "#!/bin/sh" > init
-echo "mount -t proc none /proc" > init
-echo "mount -t sysfs none /sys" > init
-echo "sysctl -w kernel.perf_event_paranoid=1" > init
-echo "echo "7" > /proc/sys/kernel/printk" > init
-echo "mknod /dev/null c 1 3" > init
-echo "chmod 666 /dev/null" > init
-echo "/bin/sh" > init
+echo "mount -t proc none /proc" >> init
+echo "mount -t sysfs none /sys" >> init
+echo "sysctl -w kernel.perf_event_paranoid=1" >> init
+echo "echo \"7\" > /proc/sys/kernel/printk" >> init
+echo "mknod /dev/null c 1 3" >> init
+echo "chmod 666 /dev/null" >> init
+echo "/bin/sh" >> init
 chmod +x init
 mkdir -p bin sbin etc proc sys usr/bin usr/sbin root
 popd
