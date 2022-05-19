6ead1de840249d6a87dbbaf361d447b7af951d8f
Architect <jbechsen@gmail.com>
2022-05-19 23:52:07 +0200

    Added init script

--- buildbusybox.sh
+++ buildbusybox.sh
@@ -15,6 +15,14 @@ make -C busybox-$BUSYBOX_VERSION install
 echo "[+] Building filesystem..."
 mkdir fs
 pushd fs
+echo "#!/bin/sh" > init
+echo "mount -t proc none /proc" > init
+echo "mount -t sysfs none /sys" > init
+echo "sysctl -w kernel.perf_event_paranoid=1" > init
+echo "echo "7" > /proc/sys/kernel/printk" > init
+echo "mknod /dev/null c 1 3" > init
+echo "chmod 666 /dev/null" > init
+chmod +x init
 mkdir -p bin sbin etc proc sys usr/bin usr/sbin root
 popd
 cp -a busybox-$BUSYBOX_VERSION/_install/* fs
